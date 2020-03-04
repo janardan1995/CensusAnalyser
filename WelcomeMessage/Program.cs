70c7a37a6a538063c3652763337fa2e79ae88609
janardan1995 <dasjanardan1995@gmail.com>
2020-03-04 09:50:28 +0530

    [JD]Add. Adding welcom Message to the master branch and push it to the remote repository

---
+++ WelcomeMessage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusInformation
+{
+    class StateCensusAnalyser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
