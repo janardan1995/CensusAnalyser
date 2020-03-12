eb055484c3c0c0fb04580fb8ddac2504466f3a0b
janardan1995 <dasjanardan1995@gmail.com>
2020-03-12 14:17:31 +0530

    Refactored Code to take in List of Census Data instead of Iterating through the File

--- CSVStatescensus.cs
+++ CSVStatescensus.cs
@@ -9,7 +9,7 @@ namespace CensusAnalyserLibrary
 {
     using System.Collections.Generic;
     using System.IO;
-
+    using System.Linq;
     /// <summary>
     /// This is CSVStateCensus class
     /// </summary>
@@ -25,31 +25,25 @@ namespace CensusAnalyserLibrary
             string type = fileInfo.Extension;
             if (type != ".CSV" && type != ".csv")
                 throw new CensusAnalyseException("INCORRECT_TYPE");
-            
-                if (!File.Exists(FilePath))
-                    throw new CensusAnalyseException("ERROR_IN_FILE_READING");
 
-                ////This line reads the CSV file and store it into the strings of array
-                string[] Lines = File.ReadAllLines(FilePath);
-                if (Lines[0] != "State,Population,AreaInSqKm,DensityPerSqKm")
-                    throw new CensusAnalyseException("HEADER_IS_NOT_FOUND");
+            if (!File.Exists(FilePath))
+                throw new CensusAnalyseException("ERROR_IN_FILE_READING");
 
-                foreach (var line in File.ReadLines(FilePath))
+            ////initialize a list
+            List<string> Lines = File.ReadAllLines(FilePath).ToList();
+            if (Lines.Contains("State,Population,AreaInSqKm,DensityPerSqKm"))
+            {
+                foreach (var line in Lines)
                 {
                     ////For delimeter
                     string[] LineCount = line.Split(delimiter);
                     if (LineCount.Length != 4 && LineCount.Length != 2)
                         throw new CensusAnalyseException("INVALID_DELIMITER");
                 }
-
-                ////store the list into iterator(IEnumerable)
-                IEnumerable<string> iterator = Lines;
-                int count = 0;
-                foreach (var item in iterator)
-                    count++;
-                return count;
-            
-           
+                return Lines.Count;
+            }
+            else
+                throw new CensusAnalyseException("HEADER_IS_NOT_FOUND");
         }
     }
 }
