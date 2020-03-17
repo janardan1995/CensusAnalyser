e1f7a92498b3c307148c020507d647ea03c81953
janardan1995 <dasjanardan1995@gmail.com>
2020-03-17 16:42:12 +0530

    [JD]Add.useCase 8 where USCode.csv file is loaded by creating a class

---
+++ USCensusCsv.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace CensusAnalyserLibrary
+{
+    public class USCensusCsv
+    {
+        public static string USCensus(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            string x = Utility.ConvertCSVFileToJsonObject(lines);
+            return x;
+        }
+    }
+}
